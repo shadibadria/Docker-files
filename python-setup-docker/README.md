a17ad71b7b1983e615ac45ff99f983495d393fc1
EC2 Default User <ec2-user@ip-172-31-43-218.ec2.internal>
2021-08-06 10:18:05 +0000

    python_attatch_mode

---
+++ python-setup-docker/README.md
@@ -0,0 +1,13 @@
+
+# simple_docker_python
+simple setup for python  attached mode using docker file
+
+# to run :
+<ul>
+  <li>install docker</li>
+  <li>RUN command : docker build -t myimage .    </li>
+   <li>RUN command : docker run -it  myimage   </li>
+
+
+
+ 
---
+++ python-setup-docker/random_app.py
@@ -0,0 +1,11 @@
+import random
+
+min_number = int(input('Please enter the min number: '))
+max_number = int(input('Please enter the max number: '))
+
+if (max_number < min_number): 
+  print('Invalid input - shutting down...')
+else:
+  rnd_number = random.randint(min_number, max_number)
+  print(rnd_number)
+
